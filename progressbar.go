aa29cf95830905aeedecd8ec12b60448da9fb545
disco07 <koneenok@outlook.fr>
2022-09-21 21:24:40 +0200

    test

--- progressbar.go
+++ progressbar.go
@@ -1,16 +1,23 @@
-package progressbar
+package main
 
 import (
 	"errors"
 	"fmt"
+	"math"
+	"strings"
+	"time"
 )
 
 type Bar struct {
 	percent int64
 	current int64
-	total   int64
 	rate    string
 	graph   string
+	config  config
+}
+
+type config struct {
+	total int64
 }
 
 func NewOption(end int64) *Bar {
@@ -22,8 +29,10 @@ func NewOption(end int64) *Bar {
 	return &Bar{
 		percent: percent,
 		current: current,
-		total:   total,
 		graph:   graph,
+		config: config{
+			total: total,
+		},
 	}
 }
 
@@ -31,23 +40,36 @@ func getPercent(current, total int64) int64 {
 	return int64((float32(current) / float32(total)) * 100)
 }
 
-func (b *Bar) PlayBar(current int) error {
-	if b.total == 0 {
+func (b *Bar) Add(current int) error {
+	if b.config.total == 0 {
 		return errors.New("the end must be greater than 0")
 	}
 
 	currentNum := int64(current)
-	b.current = currentNum
+	b.current += currentNum
 	last := b.percent
-	b.percent = getPercent(currentNum, b.total)
-	if b.percent != last && b.percent%2 == 0 {
-		b.rate += b.graph
+	if b.current > b.config.total {
+		return errors.New("current exceeds total")
 	}
-	fmt.Printf("\r[%-50s]%3d%% %8d/%d", b.rate, b.percent, b.current, b.total)
+	b.percent = getPercent(b.current, b.config.total)
+	var graphRate float64
+	graphRate = 50 / float64(b.config.total)
+	if b.percent != last {
+		b.rate += strings.Repeat(b.graph, int(math.Ceil(graphRate)))
+	}
+	fmt.Printf("\r[%-50s]%3d%% %8d/%d", b.rate, b.percent, b.current, b.config.total)
 
 	return nil
 }
 
 func Default(end int64) *Bar {
 	return NewOption(end)
+}
+
+func main() {
+	bar := Default(100)
+	for i := 0; i < int(100); i++ {
+		time.Sleep(10 * time.Millisecond)
+		bar.Add(1)
+	}
 }
