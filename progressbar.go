36489bf253155a2d2b3b1e969ef7014d0628143f
disco07 <koneenok@outlook.fr>
2022-09-26 04:00:34 +0200

    commentaire

--- progressbar.go
+++ progressbar.go
@@ -65,6 +65,7 @@ func (b *Bar) Add(current int) error {
 
 // Default is a basic usage of progress bar.
 // In parameter, the max size of things you want to view progress.
+// It returns a pointer of Bar
 func Default(end int64) *Bar {
 	return NewOption(end)
 }
